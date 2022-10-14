5b33edbf58389dbaf329ff29ede37ad583296e2f
Tatiana Pypko <tatiana.pypko@gmail.com>
2022-10-14 16:12:22 +0200

    Added homework 10

---
+++ index.js
@@ -0,0 +1,49 @@
+// ***********************
+// let result = '';
+// for (let i = 20; i <= 30; i += 0.5) {
+//   result += i + ' ';
+// }
+// console.log(result);
+
+// ************************
+// const course = 27;
+// for (let x = 10; x <= 100; x += 10) {
+//   console.log(x * course);
+// }
+
+// ***********************
+// const number = +prompt("Enter a number!", "");
+// if (number || number == 0) {
+//   for (let y = 1; y <= 100; y++) {
+//     if (y ** 2 < number) {
+//       console.log(y);
+//     }
+//   }
+// } else {
+//   alert('Not a number')
+// }
+
+// *************************
+// const numberSimple = +prompt('Enter a number!', '');
+// let count = 0;
+// for (let i = 2; i < numberSimple; i++) {
+//   if (numberSimple % i == 0) {
+//     count++;
+//   }
+// }
+// if (count || numberSimple == 0 || numberSimple == 1) {
+//   alert('Not a simple number!');
+// } else {
+//   alert('A simple number!');
+// }
+
+// ****************************
+// let numberFirst = +prompt("Enter a number!", "");
+// while (numberFirst >= 3) {
+//   numberFirst = numberFirst / 3
+// }
+// if (numberFirst == 1) {
+//     alert("The value of root of the given number is 3!");
+//   } else {
+//     alert("The value of root of the given number is NOT 3!");
+// }
