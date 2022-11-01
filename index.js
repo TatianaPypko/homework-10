f801d13dc525ef9172a3fbe24b8667a9674e9c2f
Tatiana Pypko <tatiana.pypko@gmail.com>
2022-11-01 22:41:02 +0200

    Added fixes

--- index.js
+++ index.js
@@ -13,7 +13,7 @@
 
 // ***********************
 // const number = +prompt("Enter a number!", "");
-// if (number || number == 0) {
+// if (number || number === 0) {
 //   for (let y = 1; y <= 100; y++) {
 //     if (y ** 2 < number) {
 //       console.log(y);
@@ -27,11 +27,11 @@
 // const numberSimple = +prompt('Enter a number!', '');
 // let count = 0;
 // for (let i = 2; i < numberSimple; i++) {
-//   if (numberSimple % i == 0) {
+//   if (numberSimple % i === 0) {
 //     count++;
 //   }
 // }
-// if (count || numberSimple == 0 || numberSimple == 1) {
+// if (count || numberSimple === 0 || numberSimple === 1) {
 //   alert('Not a simple number!');
 // } else {
 //   alert('A simple number!');
@@ -42,7 +42,7 @@
 // while (numberFirst >= 3) {
 //   numberFirst = numberFirst / 3
 // }
-// if (numberFirst == 1) {
+// if (numberFirst === 1) {
 //     alert("The value of root of the given number is 3!");
 //   } else {
 //     alert("The value of root of the given number is NOT 3!");
